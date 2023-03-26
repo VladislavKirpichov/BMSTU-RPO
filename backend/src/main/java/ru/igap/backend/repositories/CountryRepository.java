c9b7cd146b33f5371aa896596782d6424946bc85
VladislavKirpichov <vladislavkirpichov@gmail.com>
2023-03-26 23:45:03 +0300

    lab5

---
+++ backend/src/main/java/ru/igap/backend/repositories/CountryRepository.java
@@ -0,0 +1,12 @@
+package ru.igap.backend.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.igap.backend.models.Country;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long>
+{
+
+}
+
